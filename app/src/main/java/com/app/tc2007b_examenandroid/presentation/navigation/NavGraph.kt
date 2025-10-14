299feafea00d40bccfeb7e8a4987b789d43aee1f
Marel52 <A01424302@tec.mx>
2025-10-14 15:23:24 -0600

    feat(navigation): Implement screen navigation through the app

--- app/src/main/java/com/app/tc2007b_examenandroid/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/app/tc2007b_examenandroid/presentation/navigation/NavGraph.kt
@@ -1,9 +1,37 @@
 package com.app.tc2007b_examenandroid.presentation.navigation
 
-sealed class Screen(
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.app.tc2007b_examenandroid.presentation.screens.detail.CountryDetailScreen
+import com.app.tc2007b_examenandroid.presentation.screens.home.HomeScreen
 
-){
-    object Home : Screen ("home")
+@Composable
+fun NavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(
+                onCountryClick = { countryName ->
+                    navController.navigate("detail/$countryName")
+                }
+            )
+        }
 
-    object Detail : Screen
+        composable(
+            route = "detail/{countryName}",
+            arguments = listOf(
+                navArgument("countryName") { type = NavType.StringType }
+            )
+        ) {
+            CountryDetailScreen(
+                onBackClick = { navController.popBackStack() }
+            )
+        }
+    }
 }
